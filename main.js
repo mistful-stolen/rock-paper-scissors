2c09f50c8838a7dc126425266ed73ff4cb616083
mistful <willthedragonslayer@protonmail.com>
2022-06-08 21:46:42 +0200

    Finish computerPlay()

---
+++ main.js
@@ -0,0 +1,16 @@
+// Choices of 'rock', 'paper', 'scissors'
+let choicesOfElements = ["rock", "paper", "scissors"];
+
+function computerPlay() {
+  /*    
+        Randomly generate number
+        We use the number for the array
+        it returns that choice
+    */
+  // The computer chooses his element in short
+  let computerRandomChoice = Math.floor(Math.random() * 3);
+  let computerChoiceOfElement = choicesOfElements[computerRandomChoice];
+  return computerChoiceOfElement;
+}
+const computerSelection = computerPlay();
+const playerSelection = "rock";
